1f819528dedb1d60b46da0bf2b0ba541d858d071
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2025-07-03 20:19:00 +0200

    remove banuser module

--- Modules/BanUser/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-banuser::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('banuser.name') !!}</p>
-</x-banuser::layouts.master>
--- Modules/Core/app/Concerns/HasConsolidatedModuleConcerns.php
+++ Modules/Core/app/Concerns/HasConsolidatedModuleConcerns.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Modules\Core\Concerns;
 
 use Modules\ClassicAuth\Concerns\ClassicAuth;
-use Modules\BanUser\Concerns\HasBanChecks;
 
 trait HasConsolidatedModuleConcerns
 {
     use ClassicAuth;
-    use HasBanChecks;
 }
